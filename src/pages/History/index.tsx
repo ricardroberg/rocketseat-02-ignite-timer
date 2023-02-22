4fa1476b546696bd0a89a524a47e8949dcf14cbf
Ricard Roberg <ricardroberg@gmail.com>
2023-02-22 09:58:28 -0300

    Finalizada estilização do Histórico

--- src/pages/History/index.tsx
+++ src/pages/History/index.tsx
@@ -1,3 +1,88 @@
+import { HistoryContainer, HistoryList, Status } from "./styles";
+
 export function History() {
-  return <div>History</div>;
+  return (
+    <HistoryContainer>
+      <h1>Meu histórico</h1>
+
+      <HistoryList>
+        <table>
+          <thead>
+            <tr>
+              <th>Tarefa</th>
+              <th>Duração</th>
+              <th>Início</th>
+              <th>Status</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td>Nome da tarefa</td>
+              <td>20 minutor</td>
+              <td>há 1 mês</td>
+              <td>
+                <Status statusColor="green">Concluído</Status>
+              </td>
+            </tr>
+            <tr>
+              <td>Nome da tarefa</td>
+              <td>20 minutor</td>
+              <td>há 1 mês</td>
+              <td>
+                <Status statusColor="green">Concluído</Status>
+              </td>
+            </tr>
+            <tr>
+              <td>Nome da tarefa</td>
+              <td>20 minutor</td>
+              <td>há 1 mês</td>
+              <td>
+                <Status statusColor="yellow">Em andamento</Status>
+              </td>
+            </tr>
+            <tr>
+              <td>Nome da tarefa</td>
+              <td>20 minutor</td>
+              <td>há 1 mês</td>
+              <td>
+                <Status statusColor="yellow">Em andamento</Status>
+              </td>
+            </tr>
+            <tr>
+              <td>Nome da tarefa</td>
+              <td>20 minutor</td>
+              <td>há 1 mês</td>
+              <td>
+                <Status statusColor="yellow">Em andamento</Status>
+              </td>
+            </tr>
+            <tr>
+              <td>Nome da tarefa</td>
+              <td>20 minutor</td>
+              <td>há 1 mês</td>
+              <td>
+                <Status statusColor="red">Interrompido</Status>
+              </td>
+            </tr>
+            <tr>
+              <td>Nome da tarefa</td>
+              <td>20 minutor</td>
+              <td>há 1 mês</td>
+              <td>
+                <Status statusColor="red">Interrompido</Status>
+              </td>
+            </tr>
+            <tr>
+              <td>Nome da tarefa</td>
+              <td>20 minutor</td>
+              <td>há 1 mês</td>
+              <td>
+                <Status statusColor="red">Interrompido</Status>
+              </td>
+            </tr>
+          </tbody>
+        </table>
+      </HistoryList>
+    </HistoryContainer>
+  );
 }
